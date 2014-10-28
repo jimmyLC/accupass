35d2149e6b116b6c0bc69f8fc4b701a9ce55ca2e
Jimmy <acsd68500@gmail.com>
2014-10-28 17:09:03 +0800

    complete attendee controller CRUD

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,7 +1,9 @@
 class AttendeesController < ApplicationController
+  before_action :find_event
+  before_action :find_params, :only => [:show, :edit, :update, :destroy]
 
   def index
-    @attendees = Event.Attendee.all
+    @attendees = @event.attendees
   end
 
   def show
@@ -9,13 +11,16 @@ class AttendeesController < ApplicationController
   end
 
   def new
-    @attendee = Event.Attendee.new
+    @attendee = @event.attendees.build
   end
 
   def create
-    @attendee = Event.Attendee.new(save_params)
+    @attendee = @event.attendees.build(save_params)
     if @attendee.save
-      redirect_to :action => :index
+      redirect_to event_attendees_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -23,19 +28,28 @@ class AttendeesController < ApplicationController
   end
 
   def update
-
+    if @attendee.update(save_params)
+      redirect_to event_attendees_path
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @attendee.destroy
+    redirect_to event_attendees_path
   end
 
   private
-  def find_params
-    @attendee = Attendee.find(params[:id])
+  def find_event
+    @event = Event.find(params[:event_id])
   end
+
+  def find_params
+    @attendee = @event.attendees.find(params[:id])
+  end
+
   def save_params
     params.require(:attendee).permit(:name)
   end
 end
-
