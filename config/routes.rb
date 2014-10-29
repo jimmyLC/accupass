45de570d64c5e6292978b1a8dd142cc6f6746288
Jimmy <acsd68500@gmail.com>
2014-10-29 15:13:43 +0800

    add explore page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :events do
+
     resources :attendees
+
+    collection do
+      get :explore
+    end
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
